d3c988365885d1ad1ab703e0bfe636dc2ab01757
tarokitazawa <taro.kitazawa@dandrite.au.dk>
2025-08-23 16:12:59 +0200

    Update trim_damtag.sh

--- bulk_Dam_analysis/bulk_Dam&Tag_filter_alignment/trim_damtag.sh
+++ bulk_Dam_analysis/bulk_Dam&Tag_filter_alignment/trim_damtag.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+#### Working directory. 
+WORKDIR="/path/to/your/project"
+cd "$WORKDIR"
 
+# /path/to/your/project/trim_damtag is the output directory for trimmed fastq files
+# initialize log file
+echo "" > trim_damtag/cutadapt_output.txt
+
+# list of FASTQ R1 files (absolute or relative paths)
+file_list="/path/to/your/project/raw_fastq_filepaths_damtag.txt"
+
+while read fqfile1; do
+    fqfile2=${fqfile1%_R1_001.fastq.gz}_R2_001.fastq.gz
+    outfile1="trim_damtag/$(basename "$fqfile1")"
+    outfile2="trim_damtag/$(basename "$fqfile2")"
+
+
+	if [[ ! -e "${outfile1}" ]]
+	then 
+		# do adapter trimming
+		echo "trimming $outfile1"
+		cutadapt -a CTGTCTCTTATACACA -A CTGTCTCTTATACACA -m 10 --overlap=1 -o $outfile1 -p $outfile2 $fqfile1 $fqfile2 >> trim_damtag/cutadapt_output.txt
+
+	else 
+		echo "skipping existing $outfile1"
+	fi
+done < "$file_list"
