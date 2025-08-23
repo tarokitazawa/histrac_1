205183dfe5f04a8d5494ac654c3fdd9fb829569f
tarokitazawa <taro.kitazawa@dandrite.au.dk>
2025-08-23 16:26:01 +0200

    Update generate_mm10_GATC_bed.py

--- bulk_Dam_analysis/mm10_gatc_bed/generate_mm10_GATC_bed.py
+++ bulk_Dam_analysis/mm10_gatc_bed/generate_mm10_GATC_bed.py
@@ -1,1 +1,14 @@
+from Bio import SeqIO
+import sys
 
+fasta_file = "/path/to/your/BSgenome.Mmusculus.UCSC.mm10.fa"
+recognition_sequence = "GATC"  # Modify this as per your enzyme
+
+with open("mm10_GATC.bed", "w") as output:
+    for record in SeqIO.parse(fasta_file, "fasta"):
+        seq_str = str(record.seq)
+        start_pos = seq_str.find(recognition_sequence)
+        while start_pos != -1:
+            end_pos = start_pos + len(recognition_sequence)
+            output.write(f"{record.id}\t{start_pos}\t{end_pos}\n")
+            start_pos = seq_str.find(recognition_sequence, start_pos + 1)
